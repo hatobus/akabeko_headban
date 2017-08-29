41816931ba322608876004259b48c3dab5ea45ee
hatobus <s1240056@gmail.com>
2017-08-28 21:51:54 -0700

    upload LEDuno

---
+++ LEDSample01.ino
@@ -0,0 +1,14 @@
+#include "FastLED.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(1000);
+}
