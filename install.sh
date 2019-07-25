d6f9d4c475ea07523d706a175c4a5c2d0c3aeb37
hatobus <s1240056@gmail.com>
2019-07-25 03:48:55 +0000

    fix typo

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ if [ -e /etc/systemd/system/headbanging.service ]; then
     sudo rm -rf /etc/systemd/system/headbanging.service
 fi
 
-if [ -e /opt/akabeko/connect_Raspi/comp.py]; then
-    sudo rm -rf /opt/akabeko/connect_Raspi/comp.py
+if [ -e /opt/akabeko ]; then
+    sudo rm -rf /opt/akabeko/
 fi
 
 sudo mkdir -p /opt/akabeko
-sudo cp ./* /opt/akabeko/
-sudo cp ./headbanging.service / etc/systemd/system/headbanging.service
+sudo cp -r ./* /opt/akabeko/
+sudo cp ./headbanging.service /etc/systemd/system/headbanging.service
 
 sudo chmod 0755 /opt/akabeko/connect_Raspi/comp.py
 sudo systemctl enable headbanging.service
